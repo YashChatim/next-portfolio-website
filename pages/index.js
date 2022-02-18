ff11feb1c37b60ea337f5c242ce09bf85cbe3351
Yash Chatim <yash.chatim@gmail.com>
2022-02-18 23:45:22 +0000

    Added content on Landing page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import { Fragment } from "react";
+import Grid from '@material-ui/core/Grid';
+import { Paper } from "@material-ui/core";
 
 import MainHeader from "../components/layout/MainHeader";
+
+import classes from '../styles/landing/landing.module.css';
 
 const Index = () => {
     return (
         <Fragment>
             <MainHeader />
             <div>Welcome to Index page</div>
+            <Grid container spacing={1}>
+                <Grid item xs={6}>
+                    <Paper className={classes['paper']}></Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={`${classes['paper']} ${classes['banner-text']}`}>
+                        <h1>YASH CHATIM</h1>
+                        <h2>D E V E L O P E R  /  P R O G R A M M E R</h2>
+                        <hr /> {/* hr - thematic break in HTML e.g. topic change */}
+                    </Paper>
+                </Grid>
+            </Grid>
         </Fragment>
     );
 }
